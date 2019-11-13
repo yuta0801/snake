72e4ed7f92d9026e7411f9fc6866873d1530bdf6
yuta0801 <yuta20030801@icloud.com>
2019-11-13 18:42:43 +0900

    Use canvas instead of svg

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Konva from 'konva'
+import { Stage, Layer, Rect } from 'react-konva'
 import { useSelector } from 'react-redux'
 import { State } from '../store/state'
 import { BOARD_SIZE, CELL_SIZE, CELL_COLORS } from '../game/constants'
+
+Konva.pixelRatio = 1
 
 const Board = () => {
   const { board, player } = useSelector((state: State) => state)
@@ -9,20 +13,22 @@ const Board = () => {
   const boardSize = BOARD_SIZE * CELL_SIZE
 
   return (
-    <svg width={boardSize} height={boardSize}>
-      { board.map((col, x) => col.map((cell, y) => {
-        const here = x === player.point.x && y === player.point.y
-        const color = CELL_COLORS[here ? 1 : cell === -1 ? 2 : cell]
-        return (
-          <rect
-            x={x * CELL_SIZE} y={y * CELL_SIZE}
-            width={CELL_SIZE} height={CELL_SIZE}
-            stroke="black" fill={color}
-            key={`${x},${y}`}
-          />
-        )
-      })) }
-    </svg>
+    <Stage width={boardSize} height={boardSize}>
+      <Layer>
+        { board.map((col, x) => col.map((cell, y) => {
+          const here = x === player.point.x && y === player.point.y
+          const color = CELL_COLORS[here ? 1 : cell === -1 ? 2 : cell]
+          return (
+            <Rect
+              x={x * CELL_SIZE} y={y * CELL_SIZE}
+              width={CELL_SIZE} height={CELL_SIZE}
+              stroke="black" fill={color}
+              key={`${x},${y}`}
+            />
+          )
+        })) }
+      </Layer>
+    </Stage>
   )
 }
 
